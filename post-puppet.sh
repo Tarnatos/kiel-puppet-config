d35ccc0258a6d31fb98abbce3e71511804ec9ed9
Tarnatos <tarnatos@posteo.de>
2017-11-27 09:44:22 +0100

    Update post-puppet.sh

--- post-puppet.sh
+++ post-puppet.sh
@@ -2,9 +2,9 @@
 #https://github.com/ffnord/ffnord-puppet-gateway
 
 VPN_NUMBER=0
-DOMAIN="ahb.freifunknord.net"
-TLD=ffnord
-IP6PREFIX=fd42:eb49:c0b5:4242
+DOMAIN="kiel.freifunk.net"
+TLD=ffki
+IP6PREFIX=fda1:384a:74de:4242
 
 sed -i 's/( //;s/ )//g' /etc/ffnord
 
@@ -12,13 +12,13 @@ sed -i 's/( //;s/ )//g' /etc/ffnord
 build-firewall
 
 #fastd ovh config
-cd /etc/fastd/ffnord-mvpn/
-git clone https://github.com/Freifunk-AHB/nord-ahb-gw-peers backbone
+cd /etc/fastd/ffki-mvpn/
+git clone https://github.com/Tarnatos/kiel-gw-peers backbone
 touch /usr/local/bin/update-fastd-gw
 cat <<-EOF>> /usr/local/bin/update-fastd-gw
 #!/bin/bash
 
-cd /etc/fastd/ffnord-mvpn/backbone
+cd /etc/fastd/ffki-mvpn/backbone
 git pull -q
 EOF
 chmod +x /usr/local/bin/update-fastd-gw
